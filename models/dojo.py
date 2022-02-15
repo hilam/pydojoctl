68f09a0b4bff63996275ce3748eb690dc02996ac
Hildeberto <hildeberto@gmail.com>
2022-02-15 16:25:30 -0300

    Corrigindo inconsistências apontadas pelo prospector

--- models/dojo.py
+++ models/dojo.py
@@ -1,28 +1,36 @@
-from datetime import datetime
+from dataclasses import dataclass
 from typing import Optional
 
 
+@dataclass()
 class Evento:
+    id: Optional[int]
     nome: str
-    data: datetime
+    data: str
     duracao_prevista: int
     duracao_real: Optional[int]
     ativo: Optional[int]
 
 
+@dataclass()
 class Participante:
+    id: Optional[int]
     nome: str
     email: str
     presente: Optional[int]
 
 
+@dataclass()
 class EventoAtuacao:
+    id: Optional[int]
     evento: Evento
     participante: Participante
     ordem_sorteio: Optional[int]
 
 
+@dataclass()
 class EventoConfiguracao:
+    id: Optional[int]
     evento: Evento
     tempo_piloto: int
     tempo_audiencia: int
